16bc09157283f104e9439b8671bd77780a79acc2
Swapnil31Ingale <swapnilingale31@gmail.com>
2024-01-09 20:09:08 +0530

    Problem 7 solution

--- python_programs/Practice_Problems/Problem7.py
+++ python_programs/Practice_Problems/Problem7.py
@@ -1,1 +1,5 @@
 # Write a program that will tell whether the given year is a leap year or not.
+
+year = int(input("Please enter a year: "))
+
+print("Leap Year") if year % 4 == 0 else print("Not a leap year")

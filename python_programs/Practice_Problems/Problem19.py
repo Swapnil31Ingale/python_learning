72ec6d30a5a0a613a411bf5942c81be2194bb49f
Swapnil31Ingale <swapnilingale31@gmail.com>
2024-01-15 23:18:56 +0530

    Problem 19 solution

--- python_programs/Practice_Problems/Problem19.py
+++ python_programs/Practice_Problems/Problem19.py
@@ -1,2 +1,27 @@
 # Write a program that will take user input of (4 digits number) and 
 # check whether the number is narcissist number or not.
+
+# 1634 = 4**4 + 3**4 + 6**4 + 1**4 
+
+a = input("Enter the number: ")
+
+def getResult(a):
+    sum = 0
+    length = len(a)
+ 
+    # Traversing through the string
+    for i in a:
+ 
+        # Converting character to int
+        sum = sum + int(i) ** length
+ 
+    # Converting string to integer
+    number = int(a)
+ 
+    # Comparing number and sum
+    if (number == sum):
+        return "true: Narcissistic no", number
+    else:
+        return "false: Non-Narcissistic no", number
+  
+print(getResult(a))

fdab6863e4ca6bef9c141f884d480bd711f28644
Swapnil31Ingale <swapnilingale31@gmail.com>
2024-01-15 20:39:18 +0530

    Problem 17 solution

--- python_programs/Practice_Problems/Problem17.py
+++ python_programs/Practice_Problems/Problem17.py
@@ -5,3 +5,10 @@
 
 a = int(input("Please enter 3 digit no: "))
 
+last_digit = a % 10
+b = a // 10
+second_digit = b % 10
+first_digit = b // 10
+
+sum = ((last_digit**2) + (first_digit**2) + (second_digit**2))
+print(f"Sum of square of three digits {a} is {sum}.")

ed760bef28497c1b836cd771eacdb0cdcbcbb8a9
Swapnil31Ingale <swapnilingale31@gmail.com>
2024-01-18 21:32:57 +0530

    Problem 25 solution

--- python_programs/Practice_Problems/Problem25.py
+++ python_programs/Practice_Problems/Problem25.py
@@ -1,2 +1,13 @@
 # Write a program that can multiply 2 numbers provided by the user 
 # without using the * operator
+
+n1 = int(input("First number: "))
+n2 = int(input("Second number: "))
+
+def multiply(n1, n2):
+    result = 0
+    for i in range(n2):
+        result = result + n1
+    return result
+
+print("Multiplication: ", multiply(n1, n2))

e5a8aa1b6aa5c9dd7bbda55e6e5874e022d3ed94
Swapnil31Ingale <swapnilingale31@gmail.com>
2024-01-18 21:49:40 +0530

    Problem 26 solution

--- python_programs/Practice_Problems/Problem26.py
+++ python_programs/Practice_Problems/Problem26.py
@@ -1,2 +1,9 @@
-# Write a program that can find the factorial of a given number provided 
-# by the user.
+# Write a program that can find the factorial of a given number 
+# provided by the user.
+
+n = int(input("Number: "))
+result = 1
+
+for i in range(1, n+1):
+    result = result * i
+print(f"Factorial of {n} is {result}")

59f4dc7fbc2c8e6a05601b716ac81a2a5d7d5a98
Swapnil31Ingale <swapnilingale31@gmail.com>
2024-01-18 20:52:39 +0530

    problem 24 solution

--- python_programs/Practice_Problems/Problem24.py
+++ python_programs/Practice_Problems/Problem24.py
@@ -1,3 +1,13 @@
 # Write a program to find the sum of first n numbers, 
 # where n will be provided by the user. Eg if the user 
 # provides n=10 the output should be 55.
+
+n = int(input("Enter the number: "))
+
+sum = 0
+
+for i in range(0 ,n+1):
+    sum = sum + i
+    i = i + 1
+print(f"Sum of first {n} numbers is {sum}.")
+    

8b67087d467315a90509dfb8b067c3b4c83e60d0
Swapnil31Ingale <swapnilingale31@gmail.com>
2024-01-19 16:51:34 +0530

    Problem 28 solution

--- python_programs/Practice_Problems/Problem28.py
+++ python_programs/Practice_Problems/Problem28.py
@@ -1,1 +1,16 @@
-# Write a program to print whether a given number is prime number or not
+# Write a program to print whether a given number is prime 
+# number or not
+
+def is_prime(number):
+    if number <= 1:
+        print(f"{number} is not a prime number")
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                print(f"{number} is not a prime number")
+                break
+        else:
+            print(f"{number} is a prime number")
+
+number = int(input("Please enter a number: "))
+is_prime(number)
